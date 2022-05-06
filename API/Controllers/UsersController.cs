98b87991ae7f5a9aa4afa09fa9ab4599597d6a6d
Pooja <rdy.pooja@gmail.com>
2022-05-06 14:16:04 +0530

    AppUser Controller Created

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Getting all users
+        // api/users
+        //synchronous code
+        /*[HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var Users = _context.Users.ToList();
+            return Users;
+        }*/
+        //Asynchronous code - more scalable
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var Users = _context.Users.ToListAsync();
+            return await Users;
+        }
+
+        //Getting a specific user using Id parameter
+        // api/users/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int Id)
+        {
+            var User = _context.Users.FindAsync(Id);
+            return await User;
+        }
+    }
+}
