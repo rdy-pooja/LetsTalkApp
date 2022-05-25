f462964547f6d5aeff1eba7751e3550ec3d4aee1
Pooja <rdy.pooja@gmail.com>
2022-05-25 21:12:24 +0530

    added routing and guards

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +13,8 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,12 +22,17 @@ export class NavComponent implements OnInit {
   login()
   {
     this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
       console.log(response);
-    }, error => { console.log(error)});
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
     console.log("logged out");
   }
 }
